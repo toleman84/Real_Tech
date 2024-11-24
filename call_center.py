7f9053e412f7003324fa3dbf5411f7449691026e
toleman84 <gdponcemarsiglia@gmail.com>
2024-11-24 00:56:28 -0300

    add

---
+++ call_center.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""_summary_
+"""
+import re
+import json
+
+
+def make_all_numbers():
+    """function that make all the cell-phone numbers in UY"""
+    with open("all_number.txt", "w") as file:
+        for n in range(91, 100):
+            for i in range(1000000):
+                file.write(f"{n:03}{i:06}" + "\n")
+
+
+def forbidden_numbers():
+    """not to call !!!
+       write a valid number; like a: 099123456
+    """
+    try:
+        number = input("write a valid number; like a: 099123456 > ")
+        pattern = r"^09[1-9]\d{6}$"
+        if re.match(pattern, number):
+            with open("forbidden_numers.txt", "a") as file:
+                file.write(number + "\n")
+            print("The number has been saves succesfully.")
+        else:
+            print("Error: The number is not in the correct format (e.g., 099123456).")
+    except ValueError:
+        print("Error: input a valid number; like a: 099123456")
+
+
+def agent_numbers():
+    """this is for append a agent numers
+    """
+    try:
+        agent_name = input("Enter the agent's name: ")
+        number = input("write a valid number; like a: 099123456 > ")
+
+        # regex for validate a number
+        pattern = r"^09[1-9]\d{6}$"
+        if re.match(pattern, number):
+            try:
+                with open("agent_numbers.json", "r") as file:
+                    agent_data = json.load(file)
+            except (FileNotFoundError, json.JSONDecodeError):
+                agent_data = {}
+
+            agent_data[agent_name] = number
+
+            with open("agent_numbers.json", "w") as file:
+                json.dump(agent_data, file, indent=4)
+            print(f"The number for agent {agent_name} has been saved successfully.")
+        else:
+            print("Error: The number is not in the correct format (e.g., 099123456).")
+    except ValueError:
+        print("Error: Invalid input")
---
+++ console.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""console for Real Tech - Call Center
+
+    Returns:
+        _type_: _description_
+"""
+import cmd
+
+from call_center import forbidden_numbers, agent_numbers
+
+
+class CallCenter(cmd.Cmd):
+    """_summary_
+
+    Args:
+        cmd (_type_): _description_
+    """
+    intro = "Welcome to the Call Center! Type help or ? to list commands.\n"
+    prompt = "(call_to_all) "
+
+    def do_exit(self, arg):
+        """To exit the program
+
+        Args:
+            arg (_type_): _description_
+        """
+        print("Goodbye!")
+        return True
+
+    def do_empty_line(self):
+        """_summary_
+        """
+        return True
+    
+    def do_help(self, arg):
+        return super().do_help(arg)
+    
+    def do_EOF(self, arg):
+        """_summary_
+        """
+        print("Goodbye! [EOF]")
+        return True
+
+    def do_forbidden_numbers(self, arg):
+        """Calls the forbidden_numbers function"""
+        forbidden_numbers()
+
+    def do_agent_numbers(self, arg):
+        """this is for append a agent numers
+
+        Args:
+            arg (_type_): _description_
+        """
+        agent_numbers()
+
+
+if __name__ == "__main__":
+    CallCenter().cmdloop()
